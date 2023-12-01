88a80d8147de4c3671eff02a09cff649e0e0a572
sjmitch-git <sjmitch20@outlook.com>
2023-12-01 13:37:53 +0000

    Adds form, styles and functionality

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("Hello World!");
+const form = document.getElementById("registrationForm");
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirmPassword");
+const progressBar = document.getElementById("passwordStrength");
+const registerButton = document.getElementById("registerButton");
+
+form.addEventListener("input", handleFormInput);
+
+function handleFormInput() {
+  updateFormValidity();
+  updatePasswordStrength();
+}
+
+function updateFormValidity() {
+  const isFormValid = form.checkValidity();
+  registerButton.disabled = !isFormValid;
+}
+
+passwordInput.addEventListener("input", updatePasswordStrength);
+confirmPasswordInput.addEventListener("input", updateFormValidity);
+
+function updatePasswordStrength() {
+  const password = passwordInput.value;
+  const length = password.length;
+  const maxStrength = 100;
+
+  progressBar.value = Math.min((length / 12) * maxStrength, maxStrength);
+
+  progressBar.classList.remove("danger", "warning", "success");
+
+  if (length < 8) {
+    progressBar.classList.add("danger");
+  } else if (length >= 8 && length <= 12) {
+    progressBar.classList.add("warning");
+  } else {
+    progressBar.classList.add("success");
+  }
+}
