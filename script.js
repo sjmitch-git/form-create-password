5d39a259ad562cd1e07b91d834a2549a13058697
sjmitch-git <sjmitch20@outlook.com>
2023-12-02 13:16:58 +0000

    Adds form validation

--- script.js
+++ script.js
@@ -23,16 +23,35 @@ function updatePasswordStrength() {
   const password = passwordInput.value;
   const length = password.length;
   const maxStrength = 100;
+  const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,12}$/;
+
+  const meetsPattern = pattern.test(password);
 
   progressBar.value = Math.min((length / 12) * maxStrength, maxStrength);
 
   progressBar.classList.remove("danger", "warning", "success");
 
-  if (length < 8) {
+  if (length < 8 || length > 12) {
     progressBar.classList.add("danger");
   } else if (length >= 8 && length <= 12) {
     progressBar.classList.add("warning");
-  } else {
-    progressBar.classList.add("success");
+    if (meetsPattern) progressBar.classList.add("success");
   }
+  checkPasswordMatch();
+}
+
+function checkPasswordMatch() {
+  const password = document.getElementById("password").value;
+  const confirmPasswordInput = document.getElementById("confirmPassword");
+  const confirmPassword = confirmPasswordInput.value;
+
+  if (password === confirmPassword) {
+    confirmPasswordInput.setCustomValidity("");
+  } else {
+    confirmPasswordInput.setCustomValidity("Passwords do not match");
+  }
+}
+
+function formsubmit(event) {
+  event.preventDefault();
 }
